49cb547d1f5703bffbf9181c5740437ebc52638a
Mohammad Raaz <mohdraaz902@gmail.com>
2024-09-03 19:32:36 +0530

    Build Commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mohammadraaz:js0R1WzeupZtwpHE@cluster0.9jttv89.mongodb.net/EcomApp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
@@ -22,15 +23,31 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mohammadraaz:js0R1Wze
 const User = require('./models/User');
 const Team = require('./models/Team');
 
+//access static files
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+
+
+
+
+
+
+
+
+
+
 // Routes
 const userRoutes = require('./routes/userRoutes');
 const teamRoutes = require('./routes/teamRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/team', teamRoutes);
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
+})
 
 // Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
